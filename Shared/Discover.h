b10c48f90a3d086f15ce891450a241366e040ab9
dustyco <dustin.story@gmail.com>
2015-02-26 23:55:09 -0800

    Renamed Discover.h

---
+++ Shared/Discover.h
@@ -0,0 +1,67 @@
+#ifndef DISCOVER_H
+#define DISCOVER_H
+
+#include <QObject>
+#include <QMap>
+#include <QHostAddress>
+#include <QTimer>
+#include <QUdpSocket>
+#include "Record.h"
+
+/*
+	Not global server mode:
+	x-Announce only owned records periodically
+	 -Store all found records until they exire
+	 -Respond to only Local requests with
+	  only owned Records from same scope
+	
+	Global server mode:
+	x-Do not announce periodically
+	 -Store only Global found Records until
+	  they expire
+	 -Respond to only Global requests with
+	  Global owned and found Records
+	
+*/
+
+class Discover : public QObject
+{
+	Q_OBJECT
+
+public:
+	explicit Discover (QObject *parent = 0);
+	~Discover ();
+	void addGlobalServer (QString globalServer);
+	void addFilter (QString key, QString value);
+	void addRecord (Record record);
+	void setGlobalServerMode (bool b);
+	bool start ();
+
+signals:
+	void recordFound (Record record);
+	void recordLost (Record record);
+	void status (QString status);
+
+private slots:
+	void readDatagrams ();
+	void announceRecords ();
+	void expireRecords ();
+	void acceptRecords (QList<Record> records, bool removeThem, QString senderScope, QString senderAddress, QString senderPort);
+
+private:
+	QList<Record> ownedRecords;
+	QMap<Record,qint64> foundRecords; // Second value is timestamp of last received
+	bool running;
+	bool mServerMode;
+	bool announceNeedsResponse;
+	bool departure;
+	int announcePeriodMsec;
+	int port;
+	QHostAddress groupAddress;
+	QTimer *timer;
+	QTimer *expireTimer;
+	QUdpSocket* loopbackSocket;
+	QMap<QString, QUdpSocket*> multiSocket;
+};
+
+#endif // DISCOVER_H
